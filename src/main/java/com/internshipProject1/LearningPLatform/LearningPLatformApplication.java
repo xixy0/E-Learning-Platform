4bd4a4825b08e6bb4fe89b142eaf5ad6b923bb9d
Akhil Raju <akhil777raju@gmail.com>
2025-10-05 20:49:16 +0530

    First commit

---
+++ src/main/java/com/internshipProject1/LearningPLatform/LearningPLatformApplication.java
@@ -0,0 +1,13 @@
+package com.internshipProject1.LearningPLatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPLatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPLatformApplication.class, args);
+	}
+
+}
